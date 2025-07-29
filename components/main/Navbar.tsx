689d3fb59525779ece845dde1aaccc98ae0b7ffc
empsloc <empsloc@gmail.com>
2025-07-29 22:51:42 +0530

    empsloc username added

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            WebChain Dev
+            empsloc
           </span>
         </a>
 
